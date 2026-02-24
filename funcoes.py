c644fee286141d7a8b8cb4fb977d208d156fed6f
Rafael Gomes de Sousa <rayosklutsy@gmail.com>
2026-02-23 22:29:11 -0300

    adicionando menu

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,7 @@
 import math
 import random
+import pygame
+
 
 random.randint(0,255)
 
@@ -8,7 +10,7 @@ def random_cor():
     return cor
 
 #   Constante Gravitacional
-g=9
+g=20
 epsilon=100
 
 def distancia(m1,m2):
@@ -32,5 +34,25 @@ def gravitacao_Universal(m1, m2):
     ay1=forca_y/m1.massa
 
     return ax1, ay1
+
+
+
+
+def criar_botoes(screen, width, height, x, y, color, text):
+    pos_mouse=pygame.mouse.get_pos()
+    rect_botao = pygame.Rect(x, y, width, height)
+
+    rect_botao_borda = pygame.Rect(x, y, width+2, height+2)
+    pygame.draw.rect(screen, 'white', rect_botao_borda, width)
+    
+    pygame.draw.rect(screen, color, rect_botao, width+2)
+
+    fonte=pygame.font.Font(None, 32)
+    img_texto=fonte.render(text, True, (255, 255, 255))
+
+    rect_texto = img_texto.get_rect(center=rect_botao.center)
+    screen.blit(img_texto, rect_texto)
+
+    return rect_botao
 
 
--- main.py
+++ main.py
@@ -15,6 +15,8 @@ meio_y=heigth/2
 
 running=True
 
+state=0
+
 #m1=Corpo('Sol',posicao_x=meio_x,posicao_y=meio_y,velocidade_x=0,velocidade_y=0,massa=10000,raio=15,cor= 'yellow')
 
 
@@ -32,117 +34,139 @@ carregando=False
 massa_temp=0.1
 raio_temp=0.1
 pos_click=(0,0)
+x=pos_click[0]
+y=pos_click[1]
 
 pos_click_out=(0,0)
+
 
 while running:
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
     clock.tick(60)
     screen.fill((0, 0, 0))
+
+    if state==0:
+        btn_simular=criar_botoes(screen, 250, 75, meio_x-125, meio_y-275, 'black', 'Simular')
+        btn_simulacoes=criar_botoes(screen, 250, 75, meio_x-125, meio_y-75, 'black', 'Simulacoes Prontas')
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
 
-        if event.type==pygame.MOUSEBUTTONDOWN:
-            pos_click=event.pos
-            x=pos_click[0]
-            y=pos_click[1]
-            carregando=True
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_ESCAPE:
+                state=0
 
-        if event.type==pygame.MOUSEBUTTONUP:
-            pos_click_out=event.pos
-            x2=pos_click_out[0]
-            y2=pos_click_out[1]
+        if state==0:
+            if (event.type==pygame.MOUSEBUTTONDOWN):
+                if event.button==1:
+                    if btn_simular.collidepoint(event.pos):
+                        state=1
+                    if btn_simulacoes.collidepoint(event.pos):
+                        state=2
 
-            vx=(x2-x)*0.01
-            vy=(y2-y)*0.01
+        elif(state==1):
 
-            carregando=False
-            lista_corpos.append(Corpo('Corpo_Celeste', x, y, vx, vy, massa_temp, raio_temp, random_cor()))
-            massa_temp=1
-            raio_temp=1
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                pos_click=event.pos
+                x=pos_click[0]
+                y=pos_click[1]
+                carregando=True
 
-    if carregando==True:
-        massa_temp+=0.1
-        raio_temp+=0.1
+            if event.type==pygame.MOUSEBUTTONUP:
+                pos_click_out=event.pos
+                x2=pos_click_out[0]
+                y2=pos_click_out[1]
 
-        pygame.draw.circle(screen,(100, 100, 100), (pos_click[0],pos_click[1]),raio_temp)
+                vx=(x2-x)*0.01
+                vy=(y2-y)*0.01
+
+                carregando=False
+                lista_corpos.append(Corpo('Corpo_Celeste', x, y, vx, vy, massa_temp, raio_temp, random_cor()))
+                massa_temp=1
+                raio_temp=1
+
+    if state==1:
+        if carregando==True:
+            massa_temp+=1.015
+            raio_temp+=0.1
+            print(massa_temp)
+
+            pygame.draw.circle(screen,(100, 100, 100), (pos_click[0],pos_click[1]),raio_temp)
 
 
-    for corpo in lista_corpos:
-        if corpo.raio>150:
-            buracos_negros.append(corpo)
+        for corpo in lista_corpos:
+            if corpo.raio>150:
+                buracos_negros.append(corpo)
 
 
-    for corpo in buracos_negros:
-            corpo.cor='black'
-            corpo.raio=10
-            corpo.massa=10000
-            pygame.draw.circle(screen,(255, 255, 255), (corpo.posicao_x,corpo.posicao_y),corpo.raio+2)
+        for corpo in buracos_negros:
+                corpo.cor='black'
+                corpo.raio=10
+                corpo.massa=10000
+                pygame.draw.circle(screen,(255, 255, 255), (corpo.posicao_x,corpo.posicao_y),corpo.raio+2)
 
+                
+
+        for corpo in lista_corpos:
+            ax_tot=0
+            ay_tot=0
+
+            for outro in lista_corpos:
+                if corpo!=outro:
+                    ax,ay=gravitacao_Universal(corpo, outro)
+                    ax_tot+=ax
+                    ay_tot+=ay
+
+                    dist_x, dist_y, r=distancia(corpo,outro)
+
+                    if r<(corpo.raio+outro.raio):
+                        if(corpo.raio>=outro.raio):
+
+                            #corpo.massa+=outro.massa
+                            #corpo.raio+=outro.raio
+                            corpo.massa+=0.2
+                            corpo.raio+=0.2
+
+                            outro.massa-=0.2
+                            outro.raio-=0.2
+
+                            if(outro.raio<=3):
+                                corpos_para_remover.append(outro)
+
+                        else:
+                            #outro.massa+=corpo.massa
+                            #outro.raio+=corpo.raio
+                            outro.massa+=0.2
+                            outro.raio+=0.2
+
+                            corpo.massa-=0.2
+                            corpo.raio-=0.2
+
+                            if(corpo.raio<=3):
+                                corpos_para_remover.append(corpo)
+                        
+            for c in corpos_para_remover:
+                if c in lista_corpos:
+                    lista_corpos.remove(c)
+
+
+            corpo.atualizar_vel(ax_tot, ay_tot)
+
+        for i in lista_corpos:
+            """ if i.raio<10 and i.raio>5:
+                i.cor='blue'
+            elif (i.raio>=10 and i.raio<=50):
+                i.cor='orange'
+            elif(i.raio>50 and i.raio<=75):
+                i.cor='red'
+            elif(i.raio>75 and i.raio<=100):
+                i.cor=(193, 145, 0)
+            elif(i.raio>100):
+                i.cor='yellow' """
             
-
-    for corpo in lista_corpos:
-        ax_tot=0
-        ay_tot=0
-
-        for outro in lista_corpos:
-            if corpo!=outro:
-                ax,ay=gravitacao_Universal(corpo, outro)
-                ax_tot+=ax
-                ay_tot+=ay
-
-                dist_x, dist_y, r=distancia(corpo,outro)
-
-                if r<(corpo.raio+outro.raio):
-                    if(corpo.raio>=outro.raio):
-
-                        #corpo.massa+=outro.massa
-                        #corpo.raio+=outro.raio
-                        corpo.massa+=0.2
-                        corpo.raio+=0.2
-
-                        outro.massa-=0.2
-                        outro.raio-=0.2
-
-                        if(outro.raio<=3):
-                            corpos_para_remover.append(outro)
-
-                    else:
-                        #outro.massa+=corpo.massa
-                        #outro.raio+=corpo.raio
-                        outro.massa+=0.2
-                        outro.raio+=0.2
-
-                        corpo.massa-=0.2
-                        corpo.raio-=0.2
-
-                        if(corpo.raio<=3):
-                            corpos_para_remover.append(corpo)
-                    
-        for c in corpos_para_remover:
-            if c in lista_corpos:
-                lista_corpos.remove(c)
-
-
-        corpo.atualizar_vel(ax_tot, ay_tot)
-
-    for i in lista_corpos:
-        """ if i.raio<10 and i.raio>5:
-            i.cor='blue'
-        elif (i.raio>=10 and i.raio<=50):
-            i.cor='orange'
-        elif(i.raio>50 and i.raio<=75):
-            i.cor='red'
-        elif(i.raio>75 and i.raio<=100):
-            i.cor=(193, 145, 0)
-        elif(i.raio>100):
-            i.cor='yellow' """
-        
-        i.criar_Corpo(screen)
-
-    
+            i.criar_Corpo(screen)
 
     pygame.display.flip()
 
